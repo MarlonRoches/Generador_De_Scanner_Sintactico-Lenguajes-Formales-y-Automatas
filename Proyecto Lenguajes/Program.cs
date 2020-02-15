68857e641d69f4e9fd6ba7dd752012a2690efb65
MarlonRoches <roches856@gmail.com>
2020-02-15 11:20:06 -0600

    Obtencion de tokens listo

--- Proyecto Lenguajes/Program.cs
+++ Proyecto Lenguajes/Program.cs
@@ -14,26 +14,48 @@ namespace Proyecto_Lenguajes
             var path = Console.ReadLine();
             var reader = new StreamReader(path);
             var linea = reader.ReadLine();
-            var SETS = new Dictionary<string, char>();
+            var SETS = new Dictionary<string, List<string>>();
+            var Tokens = new Dictionary<string, List<string>>();
             while (linea != null)
             {
 
-                if (linea=="SETS")
+                if (linea == "SETS")
                 {
+                    var contador = 0;
+                    linea = reader.ReadLine().Replace("\t", "").Replace(" ", "");
+                    while (linea != "TOKENS" &&  linea != "ACTIONS")
+                    {//partimos y quitmos espacios
+                        var arreglo = linea.Split('=');
+                        SETS.Add(arreglo[0], new List<string>());
+                        var items = arreglo[1].Replace("'","");
+                        foreach (var item in items.Split('+'))
+                        {
+                            SETS[arreglo[0]].Add(item);
+                        }
+                        linea = reader.ReadLine().Replace("\t", "").Replace(" ", "");
+                    }
+
+                    if (linea == "TOKENS")
+                    {
+
+                    }
+
+                    if (linea == "ACTIONS")
+                    {
+
+                    }
+
+
 
                 }
 
 
 
+                Console.ReadLine();
+
+
+
             }
-
-
-
-            Console.ReadLine();
-
-
-
-
         }
     }
 }
