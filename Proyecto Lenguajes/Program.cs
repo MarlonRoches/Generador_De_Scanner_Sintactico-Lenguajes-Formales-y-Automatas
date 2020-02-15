07d18bab40774446ef1a64314a1aab1d5766a2ce
MarlonRoches <roches856@gmail.com>
2020-02-15 10:46:11 -0600

    Inicio De Proyecto

---
+++ Proyecto Lenguajes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Lenguajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Console.ReadLine();
+            var reader = new StreamReader(path);
+            var linea = reader.ReadLine();
+            var SETS = new Dictionary<string, char>();
+            while (linea != null)
+            {
+
+                if (linea=="SETS")
+                {
+
+                }
+
+
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
